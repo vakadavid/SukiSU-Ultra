e7b6ebd8951a9dfd724fbfca38e6d7b4f2db10df
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-11-19 07:09:30 +0100

    Update sucompat.c

--- kernel/sucompat.c
+++ kernel/sucompat.c
@@ -351,7 +351,12 @@ int ksu_handle_devpts(struct inode *inode)
 			return 0;
 
 	if (ksu_file_sid) {
-		struct inode_security_struct *sec = selinux_inode(inode);
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 1, 0) ||                           \
+	defined(KSU_OPTIONAL_SELINUX_INODE)
+		struct inode_security_struct *sec = selinux_inode(wrapper_inode);
+#else
+		struct inode_security_struct *sec = (struct inode_security_struct *)wrapper_inode->i_security;
+#endif
 		if (sec) {
 			sec->sid = ksu_file_sid;
 		}
