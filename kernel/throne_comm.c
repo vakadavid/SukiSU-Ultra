d55a3e7066e47aa949c398feff6b9fd8f1b2833c
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-12-04 18:45:07 +0100

    Update throne_comm.c

--- kernel/throne_comm.c
+++ kernel/throne_comm.c
@@ -5,6 +5,7 @@
 #include <linux/slab.h>
 #include <linux/workqueue.h>
 #include <linux/task_work.h>
+#include <linux/version.h>
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 11, 0)
 #include <linux/sched/task.h>
 #else
