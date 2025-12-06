f4731e5d02d6223b21720cbd974035d3e8eb9423
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-12-06 15:25:49 +0100

    Update pkg_observer.c

--- kernel/pkg_observer.c
+++ kernel/pkg_observer.c
@@ -8,9 +8,7 @@
 #include <linux/rculist.h>
 #include <linux/version.h>
 #include "klog.h" // IWYU pragma: keep
-#include "ksu.h"
 #include "throne_tracker.h"
-#include "throne_comm.h"
 
 #define MASK_SYSTEM (FS_CREATE | FS_MOVE | FS_EVENT_ON_CHILD)
 
